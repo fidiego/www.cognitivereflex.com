e2a9ca6ce32eee2904261e6b50a0696b17a6a2a9
Fred Diego <cognitivereflex+github@protonmail.com>
2019-07-29 14:52:35 -0400

    highlight deploy echo cmds

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 
-echo 'Installing PostCSS CLI'
+echo '*** DEPLOY: Installing PostCSS CLI'
 npm install postcss-cli
 
-echo 'Installing Tailwinds Theme Dependencies'
+echo '*** DEPLOY: Installing Tailwinds Theme Dependencies'
 cd themes/tailwinds-theme
 npm install
 
-echo 'Returning to Project Root'
+echo '*** DEPLOY: Returning to Project Root'
 cd ../../
 ls
 
-echo 'Buliding Project with Hugo'
+echo '*** DEPLOY: Buliding Project with Hugo'
 hugo
